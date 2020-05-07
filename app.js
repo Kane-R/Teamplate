06504f84f2685ef0424153dfed08b993e2c0802c
Kane Robinson <kane@kanestrades.com>
2020-05-07 21:55:03 +0800

    udated project folder/files with content from 'develop' in gitlab repo and re-installed inquirer and jest

--- app.js
+++ app.js
@@ -1,129 +1,35 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
+const path = require("path");
 const fs = require("fs");
-
-inquirer.prompt([
-    
-    // Confirm: Intro & simple opt-in or out. If out, then exit, dissconnect
-    {
-        type:"confirm",
-        name:"Create README file?",
-        message:"This is a README Generator (Awesome README Maker V1.0). Would you like to procede"
-    },
-
-    // Type?: At least one badge
-    {
-        type:"input",
-        name:"Badge 1",
-        message:"What is Badge 1?"
-    },
-
-    {
-        type:"input",
-        name:"Badge 2",
-        message:"What is Badge 2?"
-    },
-    
-    //Input: Project Title
-    {
-        type:"input",
-        name:"users name",
-        message:"what is your name?"
-    },
-
-    // Input Description
-    {
-        type:"input",
-        name:"users name",
-        message:"what is your name?"
-    },
-
-    // Table of Contents
-    {
-        type:"input",
-        name:"users name",
-        message:"what is your name?"
-    },
-
-    // Installation
-    {
-        type:"input",
-        name:"users name",
-        message:"what is your name?"
-    },
-    
-    // Usage
-    {
-        type:"input",
-        name:"users name",
-        message:"what is your name?"
-    },
-    
-    // License
-    {
-        type:"input",
-        name:"users name",
-        message:"what is your name?"
-    },
-    // Contributing
-    {
-        type:"input",
-        name:"users name",
-        message:"what is your name?"
-    },
-    // Tests
-    {
-        type:"input",
-        name:"users name",
-        message:"what is your name?"
-    },
-    // Questions
-        // User GitHub profile picture
-        // User GitHub email
-
-
-    {
-        type:"list",
-        name:"kanesList",
-        message:"This is a list",
-        choices:["list 1","list 2","list 3","list 4"]
-    },
-
-    {
-        type:"checkbox",
-        name:"kanesCheckbox",
-        message:"This is a checkbox",
-        choices:["checkbox 1","checkbox 2","checkbox  3","checkbox  4"]
-    }]
-
-    // Title
-    // Description
-    // Table of Contents
-    // Installation
-    // Usage
-    // License
-    // Contributing
-    // Tests
-    // Questions (User github profile picture, user github email)
-
-).then(answers =>{
-    console.log(answers);
-    const template = `
-    
-
-    # Badge
-    [![Generic badge](https://img.shields.io/badge/<SUBJECT>-<STATUS>-<COLOR>.svg)](https://shields.io/)
-
-    # PROJECT TITLE
-        ${answers["users name"]}
-    # DESCRIPTION
-        ${answers ["users name"]}
-    # TABLE OF CONTENTS
-        ${answers.pizzaTopping}
-    # ALL PIZZA TOPPINGS
-        ${answers["all pizza toppings"]}
-    `
-    fs.writeFile("KanosPizza.md", template, err=>{
-        if (err) throw err;
-        console.log("Success! this README has been added to your repo");
-    });
-})
+​
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+​
+const render = require("./lib/htmlRenderer");
+​
+​
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+​
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+​
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+​
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+​
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an 
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work!```
